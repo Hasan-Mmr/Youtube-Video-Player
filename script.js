8804d9a7bd5edac5a6fe6444284a1ed47ec79ed7
Hasan.Mmr <hasan.memar68@gmail.com>
2023-01-17 20:44:11 +0330

    implemented progress clicking and hovering features

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ const muteBtn = document.querySelector('.mute-btn');
 const currentTimes = document.querySelector('.current-time');
 const totalTimes = document.querySelector('.total-time');
 
+const speedBtn = document.querySelector('.speed-btn');
+
+const progress = document.querySelector('.progress')
+const progressBar = document.querySelector('.progress-bar');
+const progressIndicator = document.querySelector('.progress-current');
+
 // play and pause
 playPauseBtn.addEventListener('click', togglePlayPause);
 
@@ -22,9 +28,13 @@ function togglePlayPause() {
     video.paused ? video.play() : video.pause();
 }
 
+
+// keyboard funcionality
 document.addEventListener('keydown', e => {
+    const activeElement = document.activeElement.tagName;
+    if (activeElement === 'INPUT') return;
     const key = e.key.toLowerCase();
-    console.log(key);
+
     switch (key) {
         case 'k':
             togglePlayPause();
@@ -62,20 +72,87 @@ document.addEventListener('keydown', e => {
 })
 
 
-// subtitle
+// progress
 
-const subtitle = video.textTracks[0];
-subtitle.mode = 'hidden';
+progress.addEventListener('mousedown', handleClick);
+progress.addEventListener('mousemove', handleMouseDown);
+document.addEventListener('mouseup', e => {
+    if ( isMouseDown) handleClick(e);
+})
+document.addEventListener('mousemove', e => {
+    if ( isMouseDown) handleMouseDown(e);
+})
 
-captionBtn.addEventListener('click', toggleSubtitle)
+let isMouseDown = false;
+let isPaused = false;
+function handleClick(e) {
+    const rect = progress.getBoundingClientRect();
+    const currentX = e.pageX - rect.left;
+    const ratio =  currentX / rect.width;
+    isMouseDown = (e.buttons & 1) === 1;
 
-function toggleSubtitle() {
-    const isShowing = subtitle.mode === 'showing';
-    subtitle.mode = 
-    isShowing ? 'hidden' : 'showing';
-
-    videoContainer.classList.toggle('subtitle', !isShowing);
+    if ( isMouseDown ) {
+        isPaused = video.paused;
+        video.pause();
+    } else {
+        video.currentTime = video.duration * ratio;
+        if( !isPaused ) video.play();
+    }
+    handleMouseDown(e);
 }
+
+function handleMouseDown(e) {
+    const rect = progress.getBoundingClientRect();
+    const currentX = e.pageX - rect.left;
+    const ratio =  currentX / rect.width;
+    
+    progress.style.setProperty(`--hoverPosition`, ratio);
+
+    if ( isMouseDown ) {
+        video.currentTime = video.duration * ratio;
+    }
+}
+
+
+
+
+// document.addEventListener('mousemove', showPosition);
+// function showPosition(e) {    
+//     if ( e.target.closest('.progress') !== null) {
+//         progressBar.style.setProperty(`--hoverPosition`, `${100 - calculateRatio(e)}%`);
+//     } else {
+//         progressBar.style.setProperty(`--hoverPosition`, `${100}%`);
+//     }
+
+//     if ( e.target.closest('.progress-current') !== null && isKeyDown) {
+//         const slideIndicator = video.duration * (calculateRatio(e) / 100);
+//         console.log(slideIndicator)
+//         video.currentTime = slideIndicator;
+//     }
+// }
+
+// function calculateRatio(e) {
+//     const progressRect = progress.getBoundingClientRect();
+//     const pointerPos = e.pageX;
+//     const offset = pointerPos - progressRect.left;
+//     const ratio = (offset / progressRect.width) * 100;
+//     return ratio;
+// }
+
+// let isKeyDown = undefined;
+// document.addEventListener('mousedown', () => {
+//     isKeyDown = true;
+// })
+// document.addEventListener('mouseup', () => {
+//     isKeyDown = false;
+// })
+
+// document.addEventListener('mousedown', moveProgressIndicator);
+// function moveProgressIndicator(e) {
+//     if ( e.target.closest('.progress-current') !== null ) {
+//         progressIndicator.style.setProperty(`--currenProgress`, `${100 - calculateRatio(e)}%`)
+//     }
+// }
 
 //play pause
 video.addEventListener('click', () => {
@@ -88,6 +165,87 @@ video.addEventListener('play', () => {
 
 video.addEventListener('pause', () => {
     videoContainer.classList.add('paused');
+})
+
+// volume control
+volumeSlider.addEventListener('input', changeVolume);
+function changeVolume(e) {
+    const val = e.target.value;
+    video.volume = val;
+    video.muted = e.target.value === 0;
+}
+
+muteBtn.addEventListener('click', toggleMute);
+function toggleMute() {
+    video.muted = !video.muted;
+}
+
+video.addEventListener('volumechange', showVolumeIcon);
+function showVolumeIcon()  {
+    const vol = video.volume;
+    let volLevel;
+    if ( video.muted || vol === 0 ) {
+        volLevel =  'muted';
+    } else if ( vol > 0.5 ) {
+        volLevel =  'high';
+    } else {
+        volLevel =  'low';
+    }
+    volumeContainer.dataset.volume = volLevel;
+}
+
+// time update
+video.addEventListener('loadeddata', showTotalTime);
+function showTotalTime() {
+    totalTimes.textContent = formatTime(video.duration);
+}
+
+video.addEventListener('timeupdate', showCureentTime);
+function showCureentTime() {
+    currentTimes.textContent = formatTime(video.currentTime);
+
+    const ratio = (video.currentTime / video.duration) ;
+    progress.style.setProperty(`--videoProgress`, ratio);
+    progressIndicator.style.setProperty(`--videoProgress`, ratio);
+}
+
+const timeFormatter = new Intl.NumberFormat(undefined, {minimumIntegerDigits: 2});
+function formatTime(time) {
+    const seconds = Math.floor(time % 60);
+    const minutes = Math.floor((time  % 3600) / 60);
+    const hours = Math.floor(time / 3600);
+    if ( hours == 0  ) {
+        return `${timeFormatter.format(minutes)}:${timeFormatter.format(seconds)}`;
+    } else {
+        return `${timeFormatter.format(hours)}:${timeFormatter.format(minutes)}:${timeFormatter.format(seconds)}`;
+    }
+}
+
+function forwardBackward(duraion) {
+    video.currentTime += duraion;
+}
+
+
+// subtitle
+const subtitle = video.textTracks[0];
+subtitle.mode = 'hidden';
+
+captionBtn.addEventListener('click', toggleSubtitle)
+function toggleSubtitle() {
+    const isShowing = subtitle.mode === 'showing';
+    subtitle.mode = 
+    isShowing ? 'hidden' : 'showing';
+    videoContainer.classList.toggle('subtitle', !isShowing);
+}
+
+// playback speed
+speedBtn.addEventListener('click', () => {
+    if ( video.playbackRate < 3 ) {
+        video.playbackRate += .5;
+    } else {
+        video.playbackRate = 1;
+    }
+    speedBtn.textContent = `${video.playbackRate}x`;
 })
 
 // mini player
@@ -120,61 +278,5 @@ document.addEventListener('fullscreenchange', () => {
     }
 })
 
-// volume control
-
-volumeSlider.addEventListener('input', changeVolume);
-function changeVolume(e) {
-    const val = e.target.value;
-    video.volume = val;
-    video.muted = e.target.value === 0;
-}
-
-muteBtn.addEventListener('click', toggleMute);
-function toggleMute() {
-    video.muted = !video.muted;
-}
-
-video.addEventListener('volumechange', showVolumeIcon);
-function showVolumeIcon()  {
-    const vol = video.volume;
-    let volLevel;
-
-    if ( video.muted || vol === 0 ) {
-        volLevel =  'muted';
-    } else if ( vol > 0.5 ) {
-        volLevel =  'high';
-    } else {
-        volLevel =  'low';
-    }
-    volumeContainer.dataset.volume = volLevel;
-}
 
 
-// time update
-
-video.addEventListener('loadeddata', showTotalTime);
-function showTotalTime() {
-    totalTimes.textContent = formatTime(video.duration);
-}
-
-video.addEventListener('timeupdate', showCureentTime);
-function showCureentTime() {
-    currentTimes.textContent = formatTime(video.currentTime);
-}
-
-const timeFormatter = new Intl.NumberFormat(undefined, {minimumIntegerDigits: 2});
-
-function formatTime(time) {
-    const seconds = Math.floor(time % 60);
-    const minutes = Math.floor((time  % 3600) / 60);
-    const hours = Math.floor(time / 3600);
-    if ( hours == 0  ) {
-        return `${timeFormatter.format(minutes)}:${timeFormatter.format(seconds)}`;
-    } else {
-        return `${timeFormatter.format(hours)}:${timeFormatter.format(minutes)}:${timeFormatter.format(seconds)}`;
-    }
-}
-
-function forwardBackward(duraion) {
-    video.currentTime += duraion;
-}
